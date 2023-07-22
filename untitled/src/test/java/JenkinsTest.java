b6cf0dbc09ff28aa338e013c6273ccbaf3caf96c
ARVINDMS544 <arvindsudhi44@gmail.com>
2023-07-22 23:27:34 +0530

    First Commit

---
+++ untitled/src/test/java/JenkinsTest.java
@@ -0,0 +1,46 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class JenkinsTest{
+
+    String baseUrl = "http://demo.guru99.com/test/newtours/";
+    String expectedTitle = "Welcome: Mercury Tours";
+    String actualTitle = "";
+    public WebDriver driver;
+
+    @Test
+    public void TitleTest() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().setSize(new Dimension(1440,900));//full screen
+
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+        // launch Fire fox and direct it to the Base URL
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        Thread.sleep(5000);
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+
+        driver.close();
+
+    }
+}
